613b7dedc61dfc5351eb85dfd01a8d8eb0be21c9
zhangzhen <1557549413@qq.com>
2020-10-09 13:56:19 +0800

    日期格式

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@
  * @Autor: Pony
  * @Date: 2020-10-09 09:30:20
  * @LastEditors: Pony
- * @LastEditTime: 2020-10-09 13:44:02
+ * @LastEditTime: 2020-10-09 13:55:51
  */
 function disptime() {
     //1.组装需要的时间
     let now = new Date(); //现在的时间
     let months = now.getMonth() + 1 >= 10? `${now.getMonth() + 1}月` : `0${now.getMonth() + 1}月`;//获取月
-    let Dates = `${now.getDate()}日`;//日
+    let Dates = `${now.getDate()>=10?now.getDate():'0'+now.getDate()}日`;//日
 
     //2.获取时间
     let hours = now.getHours() >= 10 ? now.getHours() : `0${now.getHours()}`;//小时
